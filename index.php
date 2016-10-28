3394f32ed536c8820c79ea728f886ebd1fa33ba4
Matheus <klostemrk@gmail.com>
2016-10-28 10:35:12 -0200

    Classe animal
    Criação da classe

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+class Animal {
+  private $nome;
+  private $peso;
+  private $comidaPreferida;
+  public static $quantidadeAnimais = 0;
+
+  public __construct(){
+    Animal::$quantidadeAnimais++;
+  }
+
+  public function __destruct(){
+    echo $this->nome . "Foi destruído";
+  }
+
+  public function __get($atributo){
+    return $this->$atributo;
+  }
+
+  public function __set($nome,$valor) {
+    $this->$nome = $valor;
+  }
+}
+
+$a = new Animal();
+$a->nome = "Lilica";
+$a->peso = 3;
+$a->comidaPreferida = "Qualquer coisa que de pra comer";
+
+echo $a->nome . " " . $a->peso . " " . $a->comidaPreferida;
+?>
